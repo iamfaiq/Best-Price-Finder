e0b27460ae7a0910a7537683931d26fe2a40710c
iamfaiq <43803954+iamfaiq@users.noreply.github.com>
2025-10-01 21:22:29 -0400

    Quotes retrieval duration calculation
    Included functionality to calculate the time it took to retrieve all quotes in milliseconds.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        var start = LocalTime.now();
         var service = new FlightService();
-        //this returns a completable future
-        service.getQuotes()
+        //list of completablefuture of void
+        var futures = service.getQuotes()
                 .map(future -> future.thenAccept(System.out::println))
                 .collect(Collectors.toList());
+
+        CompletableFuture
+                .allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> {
+                    var end = LocalTime.now();
+                    var duration = Duration.between(start, end);
+                    System.out.println();
+                    System.out.println("Retrieved all quotes in "+duration.toMillis()+" msec.");
+                });
 
         //5 seconds delay to allow the completion of above program
         TaskDelay.simulate(5_000);
